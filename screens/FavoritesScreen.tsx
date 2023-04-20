593703f0e3815a81ccbe5674aba77178b231f5fa
Roman <r40a90@gmail.com>
2023-04-20 10:08:12 +0300

    add no data to FavoritesScreen

--- screens/FavoritesScreen.tsx
+++ screens/FavoritesScreen.tsx
@@ -1,5 +1,5 @@
 import { FC, useContext, useMemo } from "react";
-import { Text } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
 import { FavoritesContext } from "../store/context/favorites-context";
 import MealsList from "../components/MealsList/MealsList";
 import { MEALS } from "../data/dummy-data";
@@ -13,7 +13,28 @@ const FavoritesScreen: FC = () => {
     return MEALS.filter((meal) => ids.includes(meal.id));
   }, [favoriteMealsCtx]);
 
+  if (meals.length === 0) {
+    return (
+      <View style={styles.rootContainer}>
+        <Text style={styles.text}>You have no favorite yet.</Text>
+      </View>
+    );
+  }
+
   return <MealsList meals={meals} />;
 };
 
 export default FavoritesScreen;
+
+const styles = StyleSheet.create({
+  rootContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  text: {
+    fontSize: 18,
+    fontWeight: "bold",
+    color: "white",
+  },
+});
